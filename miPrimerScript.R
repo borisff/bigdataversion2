5bb9042b4e66863b5b90f789b37bb38a6b561409
borisff <b.eduardoff@gmail.com>
2019-04-12 19:34:58 -0400

    aprendimos como crear variables y a operar tablas de verdad

--- miPrimerScript.R
+++ miPrimerScript.R
@@ -1,3 +1,52 @@
 # instalar paquete ggplot2
 #install.packages('ggplot2')
 library(ggplot2)
+
+#crear variable
+variableUno <- 1
+variableDos <- 5 
+
+variableUno <- 'a'
+
+variableDos <- TRUE
+
+variableTres <- 'fui a la esquina y volvi'
+variablesTres <- 1.6
+#borre variables
+rm(variablesTres)
+rm(variableDos)
+variableUno <- variableDos
+#borre todo las variables
+rm(list = ls())
+#compare variables
+variableUno == variableDos
+#
+variableTres <- variableUno== variableDos
+variableTres <- variableUno != variableDos
+#or 
+FALSE || TRUE
+#y
+FALSE && TRUE
+
+#comparacion de variables con or
+
+variableUno < variableDos || TRUE
+
+variableCuatro <- !((variableUno < variableDos) && (variableUno > variablesTres))
+
+# <- ----asigno 
+# rm ----elimino
+# || ----este o este 
+# && ----este y este 
+# + ----sumo variables 
+# si cambio el valor de una variable este valor se pierde y no hay forma de volver a menos que corra un comando antiguo 
+ #------------------------------------------------------------------------------
+#funciones
+source("primera funcion.R")
+funcionUno()
+
+
+#source me sirve para cargar cosas que habia creado antiguamente 
+
+
+
