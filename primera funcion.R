c324c32c265bc6eea2a485272de69d89cfd1e96b
borisff <b.eduardoff@gmail.com>
2019-04-13 11:05:46 -0400

    creamos mas funciones, donde una de ellas era el resto de una divicion %%

--- primera funcion.R
+++ primera funcion.R
@@ -1,4 +1,27 @@
-funcionUno <- function(){
+funcionUno <- function(x,y){
   ## aqui va el desarrollo de la funcion 
   print ("hola mundo")
+  print(x+y)
+}
+
+#realizar el desarrollo de la velocidad de un cuerpo en relacion al tiempo 
+#function velocidad(double,double) -> double 
+#test velocidad(10,2) -> 5
+
+funcionVelocidad <- function(d,t){
+  print(d/t)
+}
+
+
+#quiero llamar el resto
+funcionResto <- function(a,b,c){
+  print((a*b)-(c*trunc((a*b)/c)))
+  
+}
+funcionRestoProf <- function(a,b,c){
+  print((a*b)%%c)
+}
+#juego mio 
+funcionsuma <- function(restultado,x){
+  print(restultado+x)
 }
